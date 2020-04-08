a6d0a095cbe42401a26cb55643d883794e8063d1
ilya1203 <ilya.sharov.0217@gmail.com>
2020-04-09 02:27:59 +0500

    new
    i wnat it all

--- board/urls.py
+++ board/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from .views import index, by_rubric, by_vid_rubric
 
 urlpatterns = [
+    path('', index, name='index'),
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
     path('<str:rubric_id>/', by_vid_rubric, name='vid_rubr'),
-    path('index/', index, name='index')
+    
     ]
